7c424c55138256282a9747b95e7f3878183f324d
Faruk Aygun <farukaygun1@outlook.com>
2023-12-28 17:10:21 +0300

    Structs.

---
+++ C# 101/struct/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace structsample;
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        
+        Rectangle rectangle = new Rectangle();
+        rectangle.ShortEdge = 3;
+        rectangle.LongEdge = 4;
+
+        Console.WriteLine("Calculate area with class : {0}",rectangle.CalculateArea());
+
+        RectangleStruct rectangleStruck;
+        rectangleStruck.LongEdge = 3;
+        rectangleStruck.ShortEdge =  4;
+
+        Console.WriteLine("Calculate area with struct : {0}",rectangleStruck.CalculateArea());
+        RectangleStructParam rectangleStructParam = new RectangleStructParam(3,4);
+        Console.WriteLine("Calculate area with struct parameter : {0}",rectangleStructParam.CalculateArea());
+    }
+}
+
+class Rectangle
+{
+    public int ShortEdge;
+    public int LongEdge;
+
+    public long CalculateArea()
+    {
+        return this.ShortEdge * this.LongEdge;
+    }
+}
+
+struct RectangleStruct
+{
+    public int ShortEdge;
+    public int LongEdge;
+
+    public long CalculateArea()
+    {
+        return this.ShortEdge * this.LongEdge;
+    }
+
+}
+
+struct RectangleStructParam
+{
+
+    public RectangleStructParam(int shortEdge, int longEdge)
+    {
+        ShortEdge = longEdge;
+        LongEdge = longEdge;
+    }
+    public int ShortEdge;
+    public int LongEdge;
+
+    public long CalculateArea()
+    {
+        return this.ShortEdge * this.LongEdge;
+    }
+
+}
